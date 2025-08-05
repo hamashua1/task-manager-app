e7df4cce2f6756912547f5be9a8a513d3ec0cba0
hamashua1 <joshuahamashua@gmail.com>
2025-08-05 01:38:54 +0000

    file updated

--- controllers/logPageController.js
+++ controllers/logPageController.js
@@ -1,5 +1,7 @@
 import { tdModel } from "../models/td.js"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
 export const signUp = async(req,res)=>{
     try{
@@ -13,5 +15,24 @@ export const signUp = async(req,res)=>{
         res.status(401).json({message:'credentials failed to be added to database'})
     }
     }
+
+export const signIn = async(req,res)=>{
+    const {email,password} = req.body
+    const results = await tdModel.findOne({email})
+    if(!results){
+    return res.status(401).json({message:'email not found in our system, sign up'})
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, results.password)
+    if(!isPasswordCorrect){
+    return res.status(401).json({message:'password miss-matched!'})
+}
+const token = jwt.sign ({id:results._id}, process.env.JWT_SECRET, {expiresIn:'1h'})
+res.cookie('token',token, {
+    httpOnly: true,
+    secure:false,
+    samesite: 'lax',
+    maxAge: 60 * 60 * 1000 })
+   res.status(200).json({message:'sign in successful',results, isPasswordCorrect})
+    }    
 
 
--- routes/loginPage.js
+++ routes/loginPage.js
@@ -1,33 +1,11 @@
 import express from 'express'
-import { signUp } from '../controllers/logPageController.js'
-import { tdModel } from '../models/td.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import { signIn, signUp } from '../controllers/logPageController.js'
+
+
 const router = express.Router()
 
 router.post('/api/todo/signUp', signUp)
-router.post('/api/todo/signIn', async(req,res)=>{
-    const {email,password} = req.body
-    const results = await tdModel.findOne({email})
-    if(!results){
-    return res.status(401).json({message:'email mismatch'})
-    }
-    const isPasswordCorrect = await bcrypt.compare(password, results.password)
-    if(!isPasswordCorrect){
-    return res.status(401).json({message:'password miss-matched!'})
-}
-const token = jwt.sign ({id:results._id}, process.env.JWT_SECRET, {expiresIn:'1h'})
-res.cookie('token',token, {
-    httpOnly: true,
-    secure:false,
-    samesite: 'lax',
-    maxAge: 60 * 60 * 1000 })
-   res.status(200).json({message:'sign in successful',results, isPasswordCorrect})
-    })
-
-
-
-
+router.post('/api/todo/signIn', signIn)
 
 
 export default router
